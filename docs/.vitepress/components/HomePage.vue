e95991d48921d644742a9f24a59ba2e638beda31
teplostanski <igor@teplostanski.me>
2026-01-21 21:14:12 +0300

    v0.20.0-beta.4

--- docs/.vitepress/components/HomePage.vue
+++ docs/.vitepress/components/HomePage.vue
@@ -1,78 +1,17 @@
-<template>
-  <div class="homepage-container">
-    <div class="homepage-block">
-      <h2 class="voidzero-title">Создано</h2>
-      <a
-        class="voidzero homepage-logo"
-        href="https://voidzero.dev/"
-        target="_blank"
-        title="voidzero.dev"
-        alt="VoidZero"
-      />
-    </div>
+<script setup lang="ts">
+import Users from './Users.vue'
+import Sponsors from './Sponsors.vue'
+</script>
 
-    <div class="homepage-sponsors">
-      <img
-        src="https://cdn.jsdelivr.net/gh/sxzz/sponsors/sponsors.wide.svg"
-        alt="Sponsors"
-      />
-    </div>
+<template>
+  <div class="home-page">
+    <Users />
+    <Sponsors />
   </div>
 </template>
 
-<style scoped>
-.homepage-container {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-}
-
-.homepage-block {
-  margin-top: 20px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  gap: 8px;
-}
-
-.homepage-logo {
-  display: block;
-  height: 75px;
-  width: 300px;
-}
-
-.homepage-sponsors {
-  margin-top: 12px;
-}
-
-.homepage-version {
-  color: #ff7e17;
-}
-
-.t-repo-link {
-  text-decoration: underline;
-  color: #ff7e17;
-}
-
-.voidzero-title {
-  line-height: 32px;
-  font-size: 24px;
-  font-weight: 600;
-}
-
-.voidzero {
-  background: url(https://cdn.jsdelivr.net/gh/voidzero-dev/community-design-resources/brand-assets/voidzero/voidzero-logo-dark.svg)
-    no-repeat center;
-  background-size: contain;
-}
-
-.dark .voidzero {
-  background-image: url(https://cdn.jsdelivr.net/gh/voidzero-dev/community-design-resources/brand-assets/voidzero/voidzero-logo-light.svg);
-}
-
-:deep(svg) {
-  max-width: 100%;
-  height: auto;
+<style>
+.home-page a {
+  text-decoration: none;
 }
 </style>
